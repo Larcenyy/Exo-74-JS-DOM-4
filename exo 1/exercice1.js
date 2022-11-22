4e8fa0bfde0bdb9f91686077577fa3122fa6e996
Rémy <remyc59440@hotmail.com>
2022-11-22 10:56:48 +0100

    exo 74 ok

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,11 +1,14 @@
 //Votre code ci dessous
 
+let monImg = document.getElementById("cat-pic");
+let monChat = document.getElementById("cat-chat");
 
+function miaule() {
+    monChat.innerText = "Miam miam neggae"
+}
 
-
-
-
-
-
-
+monImg.addEventListener("click", function (){
+    miaule()
+    console.log("bouton déclancher");
+})
 
